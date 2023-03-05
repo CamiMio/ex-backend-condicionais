c89307883221b7f7489efa15b03d4a8f1677a6c9
Camila Mio <camilamio25@gmail.com>
2023-03-05 20:19:27 -0300

    exercicio 02

---
+++ exercicio02.js
@@ -0,0 +1,13 @@
+const jogada1 = 5
+const jogada2 = 2
+let somaJogadas = (jogada1 + jogada2) ;
+
+if (somaJogadas % 2 == 0) {
+    console.log("par");
+} else {
+    console.log("impar");
+}
+
+
+    
+
