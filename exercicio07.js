cb9b2b68af3396a80018c85bae0c4b041b0b2272
Camila Mio <camilamio25@gmail.com>
2023-03-06 20:02:41 -0300

    exercico 07 corrigido

--- exercicio07.js
+++ exercicio07.js
@@ -1,6 +1,6 @@
 const aposentada = false;
 const portadoraDeDoenca = false;
-const totalDeRendimentos = 2700000; //emCentavos
+const totalDeRendimentos = 3000000; //emCentavos
 
 if (aposentada || portadoraDeDoenca ){
     console.log("ISENTA");
@@ -8,7 +8,7 @@ if (aposentada || portadoraDeDoenca ){
 } else if ( totalDeRendimentos <= 2855970 ) {
     console.log("VAZA LEAO! JA TA DIFICIL SEM VOCÊ");
 
-} else if ( totalDeRendimentos > 2855970) {
+} else {
     console.log("PEGA LEAO");
 }
 
