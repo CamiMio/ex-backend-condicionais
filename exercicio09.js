1456e49ef42f4bd2c00170c647600ecc5a5e603d
Camila Mio <camilamio25@gmail.com>
2023-03-06 20:16:58 -0300

    exercicio 09

---
+++ exercicio09.js
@@ -0,0 +1,17 @@
+const nota = 8.5
+
+if (nota >= 9 && nota <= 10 ) {
+    console.log( "O estudante teve conceito A");
+
+} else if (nota >= 8 && nota <= 8.9) {
+    console.log( "O estudante teve conceito B");
+
+} else if (nota >= 6 && nota <= 7.9) {
+    console.log( "O estudante teve conceito C");
+
+} else if (nota >= 4 && nota <= 5.9) {
+    console.log( "O estudante teve conceito D");
+
+} else {
+    console.log( "O estudante teve conceito E");
+}
