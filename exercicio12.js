ba8381a3e3b8347e0a5c54a322217c2fa6640436
Camila Mio <camilamio25@gmail.com>
2023-03-06 20:52:26 -0300

    exercicio 12

---
+++ exercicio12.js
@@ -0,0 +1,23 @@
+const diaDaSemana = 3;
+
+if (diaDaSemana === 1) {
+    console.log("segunda-feira");
+
+} else if (diaDaSemana === 2) {
+    console.log("terça-feira");
+
+} else if (diaDaSemana === 3) {
+    console.log("quarta-feira");
+
+} else if (diaDaSemana === 4) {
+    console.log("quinta-feira");
+
+} else if (diaDaSemana === 5) {
+    console.log("sexta-feira");
+
+} else if (diaDaSemana === 6) {
+    console.log("sabádo");
+
+} else {
+    console.log("domingo");
+} 
